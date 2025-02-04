564886ce5d7a0e34bcaa522b1ac4ca6fb5d89075
theblacksnow95 <enrique.murillo24@gmail.com>
2025-02-04 17:25:37 +0100

    sdc

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,11 +6,33 @@
 /*   By: emurillo <emurillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 14:56:48 by emurillo          #+#    #+#             */
-/*   Updated: 2025/02/03 18:02:22 by emurillo         ###   ########.fr       */
+/*   Updated: 2025/02/04 13:59:48 by emurillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
+
+int	num_check(char *av)
+{
+	int	i;
+
+	i = 0;
+	while (av[i])
+	{
+		if (av[i] == '-' || av[i] == '+')
+			i++;
+		while (av[i])
+		{
+			if (!(av[i] <= '0' && av[i] >= '9'))
+				return (1);
+			i++;
+		}
+	}
+	if (i == 1 && (av[i] == '-' || av[i] == '+'))
+		return (1);
+	return (0);
+}
+
 
 int	check_args(int ac, char **av)
 {
@@ -26,7 +48,10 @@ int	check_args(int ac, char **av)
 		while (av[i][j])
 		{
 			if (!(av[i][j] >= '0' && av[i][j] <= '9'))
+			{
+				ft_printf("Not numeric\n");
 				return (1);
+			}
 			j++;
 		}
 		if (j == 1 && (av[i][0] == '-' || av[i][0] == '+'))
@@ -39,7 +64,7 @@ int	check_args(int ac, char **av)
 
 int	main(int ac, char **av)
 {
-	if (ac >= 2 && check_args(ac, av))
+	if (ac >= 2 && check_args(ac, av) == 0)
 	{
 		parsing_args(av, ac);
 	}
